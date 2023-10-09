4a6dd90c815de5bd73861f968957334dbcaf68ec
SavindiRashmika <rashmikawadanambi>
2023-10-09 11:11:20 +0530

    customer mange added

---
+++ Assignment/JS/SPA/db/db.js
@@ -0,0 +1,5 @@
+var customerDB = [
+    {id: "C00-001", name: "Savindi Rashmika", address: "Matara", salary: 45000},
+    {id: "C00-002", name: "Nethmi Thathsarani", address: "Matara", salary: 35000},
+    {id: "C00-003", name: "Lasini Prebashi", address: "Ahangam", salary: 35000}
+];
