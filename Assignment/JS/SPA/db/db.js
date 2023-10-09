de65ff92f972b2fde4c578a064f1d1fd151aa287
SavindiRashmika <rashmikawadanambi>
2023-10-10 00:01:13 +0530

    pos item

--- Assignment/JS/SPA/db/db.js
+++ Assignment/JS/SPA/db/db.js
@@ -3,3 +3,9 @@ var customerDB = [
     {id: "C00-002", name: "Nethmi Thathsarani", address: "Matara", salary: 35000},
     {id: "C00-003", name: "Lasini Prebashi", address: "Ahangam", salary: 35000}
 ];
+
+var itemDB = [
+    {code:"I00-001",description:"Lux",qtyOnHand: 100,unitPrice: 145.00},
+    {code:"I00-002",description:"Sunlight",qtyOnHand: 150,unitPrice: 345.00},
+    {code:"I00-003",description:"Light Boy",qtyOnHand: 400,unitPrice: 245.00}
+];
